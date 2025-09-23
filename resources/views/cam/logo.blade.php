7da0c548f40892a91eea946a7ee6f0ef1059ef72
Gustavo Caiano <gustavocaiano@gmail.com>
2025-09-23 18:42:44 +0100

    styling

--- resources/views/cam/logo.blade.php
+++ resources/views/cam/logo.blade.php
@@ -1,1 +1,3 @@
-{!! file_get_contents(public_path('logos/nf-logo-red-stroke.svg')) !!}
+<div class="flex items-center">
+    {!! file_get_contents(public_path('logos/nf-logo-red-stroke.svg')) !!}
+</div>
